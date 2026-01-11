0398e7069121d2b305efe31fdb99d0512bc492b7
Laxman-Male <laxmanmale122@gmail.com>
2026-01-11 21:36:04 +0530

    feat/loginPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ProfileComponent } from './account/profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { OtpScreenComponent } from './login/otp-screen/otp-screen.component';
+import { NameScreenComponent } from './login/name-screen/name-screen.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +13,17 @@ export const routes: Routes = [
     {
         path:'profile',
         component:ProfileComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'otp-screen',
+        component:OtpScreenComponent
+    },
+    {
+        path:'name-screen',
+        component:NameScreenComponent
     }
 ];
